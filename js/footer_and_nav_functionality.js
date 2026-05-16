39f909dfdb0b8a89ce1ba387c115f1b1c53b4d56
filez34 <sophiakotova@gmail.com>
2026-05-16 15:52:28 +0200

    dataset first commit
    open for edit of fluff

--- js/footer_and_nav_functionality.js
+++ js/footer_and_nav_functionality.js
@@ -1,13 +1,17 @@
 /* i want nav and footer be insertable in every page */
+
+/*find holders */
 function load_nav_and_footer() {
   const nav_holder = document.querySelector("nav");
   const footer_holder = document.querySelector("footer");
 
+  /* fetch the files that have nav and footer inner html and if failed give error */
   fetch("nav.html")
     .then((response) => {
       if (!response.ok) throw new Error("Nav not found");
       return response.text();
     })
+    /*take inner html of nav.js and put into the holder*/
     .then((data) => {
       nav_holder.innerHTML = data;
     })
@@ -20,6 +24,7 @@ function load_nav_and_footer() {
       if (!response.ok) throw new Error("Footer not found");
       return response.text();
     })
+    /*same as above*/
     .then((data) => {
       footer_holder.innerHTML = data;
     })
@@ -27,4 +32,6 @@ function load_nav_and_footer() {
       console.error("Error", error);
     });
 }
+
+// function call, DO NOT DELETE
 load_nav_and_footer();
--- js/member_functionality.js
+++ js/member_functionality.js
@@ -12,28 +12,29 @@ ages
 year of release
 series
 
-these would be a hidden image metadata
+these are in data.js (i done goofed with image metadata uwu)
 
 when on catalogue and product page members, i want metadata to be pulled from image to fill its sibling text boxes with relevant data
 when on cart, i want only name and price displayed
 
-the data is to be breaken down into groups:
+important data:
 name
 price
+activity filter
 is it a bundle?
 if yes: member links
 fluff-data (includes info abt everything)*/
 
-window.onload = function(){
-    const data = JSON.parse(localStorage.getItem("clicked_item"));
+window.onload = function () {
+  const data = JSON.parse(localStorage.getItem("clicked_item"));
 
-    if (data.is_bundle){
-        //load standard shit
-        // +members
-    } else if (!data.is_bundle){
-        //load standard shit
-    } else alert("shit is broken") //we presume shit is broken
-}
+  if (data.is_bundle) {
+    //load standard shit
+    // +members
+  } else if (!data.is_bundle) {
+    //load standard shit
+  } else alert("Error"); //we presume shit is broken
+};
 
 // when we press add we add the shit from clicked_item to cart array as an item
 // first we parse the cart array, then we push new item and then we stringify the cart back
@@ -41,10 +42,9 @@ let items_in_cart = json.parse(localStorage.getItem("items_in_cart")) || [];
 
 add_button.addEventListener("click", add_function);
 
-function add_button(){
-    //adding all the data
-    //let item = {all the data};
-    //items_in_cart.push(item);
-    localStorage.setItem("items_in_cart", JSON.stringify(items_in_cart));
+function add_button() {
+  //adding all the data
+  //let item = {all the data};
+  //items_in_cart.push(item);
+  localStorage.setItem("items_in_cart", JSON.stringify(items_in_cart));
 }
-
